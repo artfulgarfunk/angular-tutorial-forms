670c03b93a6f6660e2c42dfa45fcf80948cdff2b
Jack Henderson <jack.henderson@arctoris.com>
2017-10-27 11:27:25 +0100

    reactive form access control with get(formControlName)

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  // gives us a property which will hold the controls gropu
+
+// runs before template is rendered
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      // wrap as string to protect during minification
+      'username': new FormControl('default username jerry', Validators.required),
+      'email': new FormControl('jenkins@jenkins.jenkins', [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
